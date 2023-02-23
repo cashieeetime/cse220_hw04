cd5d1f93187674dc24b76017616ca96ef0e57975
cashieeetime <cashelcreates@gmail.com>
2023-02-22 21:42:50 -0500

    in progress update

--- largest.c
+++ largest.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-  
+  int idx, size, max;
+  size = 6;
+  max = 0;
+  int numbers[size];
+
+  for (idx=0; idx<size; idx++) {
+    scanf("%d", &numbers[idx]);
+  }
+
+  for (idx=size - 1; idx>=0; idx--) {
+    if (numbers[idx] > max) {
+      max = numbers[idx];
+    }
+  }
+
+  printf("\n%d", max);
   return 0;
 }
