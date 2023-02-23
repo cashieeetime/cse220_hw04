3059effc724e4328f4e59b62bba067e3b0cd6713
cashieeetime <cashelcreates@gmail.com>
2023-02-22 19:09:42 -0500

    assignment complete

--- even.c
+++ even.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-  
-  return 0;
+  int num, sqrt, i;
+  scanf("%d", &num);
+  i = 2;
+  sqrt = 0;
+  while (sqrt < num) {
+    sqrt = i*i;
+    if (sqrt <= num) {
+      printf("\n%d\n", sqrt);
+      i += 2; }
+  } return 0;
 }
